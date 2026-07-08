1a7a59618827ce895d0a3fc8ce70979cf74ad983
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-09 00:52:39 +0530

    Time: 1 ms (99.88%), Space: 44.2 MB (30.76%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -1,67 +1,107 @@
+
+//(1)
+// class Solution {
+//     public int[] shortestToChar(String s, char c) {
+
+//         List<Integer> list = new ArrayList<>();
+//         int n = s.length();
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(s.charAt(i) == c)
+//             {
+//                 list.add(i);
+//             }
+//         }
+
+//         int[] arr = new int[n];
+//         if(list.size() == 1)
+//         {
+//             int index = list.get(0);
+//             for(int i = 0; i < n; i++)
+//             {
+
+//                 arr[i] = Math.abs(i - index);
+
+//             }
+//             return arr;
+
+//         }
+//         int lF = 0;
+//         int lS = 1;
+//         int lFV = list.get(lF);
+//         int lSV = list.get(lS);
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(i == lFV || i == lSV)
+//             {
+//                 arr[i] = 0;
+//             }
+
+//             else if(i > lSV && lS < list.size() - 1)
+//             {
+//                 lFV = list.get(++lF);
+//                 lSV = list.get(++lS);
+//                 arr[i] = Math.min(Math.abs(i - lFV), Math.abs(i - lSV));
+//             }
+//             else if(i > lFV && i < lSV)
+//             {
+//                 arr[i] = Math.min(Math.abs(i - lFV), Math.abs(i - lSV));
+//             }
+//             else if(i <= lFV && lF == 0)
+//             {
+//                 arr[i] = Math.abs(i - lFV);
+//             }
+//             else if(i >= lSV && lS == list.size() - 1)
+//             {
+//                 arr[i] = Math.abs(i - lSV);
+//             }
+
+//         }
+
+//         return arr;
+
+//     }
+// }
+
+//(2)
 class Solution {
     public int[] shortestToChar(String s, char c) {
 
-        List<Integer> list = new ArrayList<>();
         int n = s.length();
-        for(int i = 0; i < n; i++)
-        {
-            if(s.charAt(i) == c)
-            {
-                list.add(i);
+        int[] ans = new int[n];
+
+        // A very small value so that initially
+        // no character has been seen on the left.
+        int prev = -n;
+
+        // ---------------- Left to Right ----------------
+        // Store distance from nearest left occurrence.
+        for (int i = 0; i < n; i++) {
+
+            // Update previous occurrence of c
+            if (s.charAt(i) == c) {
+                prev = i;
             }
+
+            ans[i] = i - prev;
         }
 
-        int[] arr = new int[n];
-        if(list.size() == 1)
-        {
-            int index = list.get(0);
-            for(int i = 0; i < n; i++)
-            {
+        // A very large value so that initially
+        // no character has been seen on the right.
+        prev = 2 * n;
 
-                arr[i] = Math.abs(i - index);
+        // ---------------- Right to Left ----------------
+        // Compare with nearest right occurrence.
+        for (int i = n - 1; i >= 0; i--) {
 
-
-
-
-            }
-            return arr;
-            
-
-        }
-        int lF = 0;
-        int lS = 1;
-        int lFV = list.get(lF);
-        int lSV = list.get(lS);
-        for(int i = 0; i < n; i++)
-        {
-            if(i == lFV || i == lSV)
-            {
-                arr[i] = 0;
-            }
-            
-            else if(i > lSV && lS < list.size() - 1)
-            {
-                lFV = list.get(++lF);
-                lSV = list.get(++lS);
-                arr[i] = Math.min(Math.abs(i - lFV), Math.abs(i - lSV));
-            }
-            else if(i > lFV && i < lSV)
-            {
-                arr[i] = Math.min(Math.abs(i - lFV), Math.abs(i - lSV));
-            }
-            else if(i <= lFV && lF == 0)
-            {
-                arr[i] = Math.abs(i - lFV);
-            }
-            else if(i >= lSV && lS == list.size() - 1)
-            {
-                arr[i] = Math.abs(i - lSV);
+            // Update next occurrence of c
+            if (s.charAt(i) == c) {
+                prev = i;
             }
 
-
+            ans[i] = Math.min(ans[i], prev - i);
         }
 
-        return arr;
-        
+        return ans;
     }
 }
