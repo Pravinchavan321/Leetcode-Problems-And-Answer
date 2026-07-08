2f4c2817ca79bf99055ed0558af1d89865226e20
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-08 23:16:41 +0530

    Time: 17 ms (50.27%), Space: 51 MB (57.1%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,43 +1,75 @@
+
+//(1)
+
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+
+//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+//         int n = matrix.length;
+//         int count = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 int elem = matrix[i][j];
+//                 if (count < k) {
+//                     maxHeap.offer(elem);
+//                     count++;
+
+//                 } else if (!maxHeap.isEmpty()) {
+
+//                     if (elem <= maxHeap.peek()) {
+//                         maxHeap.poll();
+//                         maxHeap.offer(elem);
+
+//                     }
+
+//                 }
+
+//             }
+//         }
+
+//         return maxHeap.poll();
+
+//     }
+// }
+
+//(2)
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
 
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-
         int n = matrix.length;
-        int count = 0;
+
+    
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+                (a, b) -> a[0] - b[0]);
+
         
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                int elem = matrix[i][j];
-                if(count < k)
-                {
-                    maxHeap.offer(elem);
-                    count++;
-
-                }
-                else if(!maxHeap.isEmpty())
-                {
-                    
-                    if(elem <= maxHeap.peek())
-                    {
-                        maxHeap.poll();
-                        maxHeap.offer(elem);
-
-                    }
-
-                }
-
-
-
-            }
+        for (int i = 0; i < n; i++) {
+            minHeap.offer(new int[] { matrix[i][0], i, 0 });
         }
 
+        while (k > 1) {
 
-        return maxHeap.poll();
+            int[] curr = minHeap.poll();
 
-        
+            int value = curr[0];
+            int row = curr[1];
+            int col = curr[2];
+
+            
+            if (col + 1 < n) {
+                minHeap.offer(new int[] {
+                        matrix[row][col + 1],
+                        row,
+                        col + 1
+                });
+            }
+
+            k--;
+        }
+
+        return minHeap.peek()[0];
     }
 }
