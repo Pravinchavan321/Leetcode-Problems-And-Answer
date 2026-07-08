f76da1c2a6bd7862b0e163027a4f416a37dfb20e
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-08 21:43:39 +0530

    Time: 1 ms (100%), Space: 42.5 MB (99.55%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,39 +1,94 @@
+
+                        //(1)
+
+// class Solution {
+//     public String toGoatLatin(String sentence) {
+
+//         String[] arr = sentence.split("\\s+");
+
+//         int n = arr.length;
+//         StringBuilder sRes = new StringBuilder();
+
+//         for (int i = 0; i < n; i++) {
+
+//             StringBuilder sb = new StringBuilder(arr[i]);
+//             char c = arr[i].charAt(0);
+
+//             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+//                     c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+
+//                 sb.append("ma");
+
+//             } else {
+
+//                 sb.deleteCharAt(0);
+//                 sb.append(c);
+//                 sb.append("ma");
+//             }
+
+//             for (int j = 0; j <= i; j++) {
+//                 sb.append('a');
+//             }
+
+//             sRes.append(sb);
+
+//             if (i != n - 1) {
+//                 sRes.append(" ");
+//             }
+//         }
+
+//         return sRes.toString();
+//     }
+// }
+
+                        //(2)
+
 class Solution {
     public String toGoatLatin(String sentence) {
 
-        String[] arr = sentence.split("\\s+");
+        StringBuilder res = new StringBuilder();
+        int n = sentence.length();
+        int word = 1;
+        int i = 0;
 
-        int n = arr.length;
-        StringBuilder sRes = new StringBuilder();
+        while (i < n) {
 
-        for (int i = 0; i < n; i++) {
+            int start = i;
+            char first = sentence.charAt(i);
 
-            StringBuilder sb = new StringBuilder(arr[i]);
-            char c = arr[i].charAt(0);
+            while (i < n && sentence.charAt(i) != ' ') {
+                i++;
+            }
 
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+            if ("AEIOUaeiou".indexOf(first) != -1) {
 
-                sb.append("ma");
+                for (int j = start; j < i; j++) {
+                    res.append(sentence.charAt(j));
+                }
 
             } else {
 
-                sb.deleteCharAt(0);
-                sb.append(c);
-                sb.append("ma");
+                for (int j = start + 1; j < i; j++) {
+                    res.append(sentence.charAt(j));
+                }
+
+                res.append(first);
             }
 
-            for (int j = 0; j <= i; j++) {
-                sb.append('a');
+            res.append("ma");
+
+            for (int j = 0; j < word; j++) {
+                res.append('a');
             }
 
-            sRes.append(sb);
+            word++;
 
-            if (i != n - 1) {
-                sRes.append(" ");
+            if (i < n) {
+                res.append(' ');
+                i++;
             }
         }
 
-        return sRes.toString();
+        return res.toString();
     }
 }
