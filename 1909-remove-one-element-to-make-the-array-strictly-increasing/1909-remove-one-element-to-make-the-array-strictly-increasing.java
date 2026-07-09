778590b1b57a2994166d52866e81928542ffccf4
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-09 22:37:35 +0530

    Time: 0 ms (100%), Space: 44.7 MB (18.87%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
@@ -1,37 +1,70 @@
+                                //(1)
+// class Solution {
+//     public boolean canBeIncreasing(int[] nums) {
+//         int n = nums.length;
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             int prev = 0;
+//             boolean bool = false;
+
+//             for (int j = 0; j < n; j++) {
+//                 if (i == j && i == 0) {
+//                     prev = 0;
+
+//                 } else if (i == j) {
+//                     prev = nums[i - 1];
+//                 } else if (i != j && nums[j] > prev) {
+//                     prev = nums[j];
+//                     bool = true;
+
+//                 } else {
+//                     bool = false;
+//                     break;
+
+//                 }
+
+//             }
+
+//             if (bool) {
+//                 return true;
+
+//             }
+
+//         }
+
+//         return false;
+
+//     }
+// }
+
+                    //(2)
+
 class Solution {
     public boolean canBeIncreasing(int[] nums) {
-        int n = nums.length;
+
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            int prev = 0;
-            boolean bool = false;
 
-            for (int j = 0; j < n; j++) {
-                if (i == j && i == 0) {
-                    prev = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
 
-                } else if (i == j) {
-                    prev = nums[i - 1];
-                } else if (i != j && nums[j] > prev) {
-                    prev = nums[j];
-                    bool = true;
+            if (nums[i] >= nums[i + 1]) {
 
-                } else {
-                    bool = false;
-                    break;
+                count++;
 
+                if (count > 1) {
+                    return false;
                 }
 
+                // Decide which element to "remove"
+                if (i > 0 && nums[i - 1] >= nums[i + 1]) {
+                    // Remove nums[i + 1]
+                    nums[i + 1] = nums[i];
+                }
+                // Else remove nums[i]
+                // No assignment is needed because future comparisons
+                // naturally skip over nums[i].
             }
-
-            if (bool) {
-                return true;
-
-            }
-
         }
 
-        return false;
-
+        return true;
     }
 }
