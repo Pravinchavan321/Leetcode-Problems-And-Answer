1d4b8bddedfef7e1bba3cde9743ac656cc371be2
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-10 16:16:57 +0530

    Time: 18 ms (58.26%), Space: 47.7 MB (25.51%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,32 +1,62 @@
+                    //(1)
+
+// class Solution {
+//     public int mostFrequentEven(int[] nums) {
+
+//         int n = nums.length;
+//         Map<Integer, Integer> hashMap = new HashMap<>();
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+//             if (b[1] == a[1]) {
+//                 return a[0] - b[0];
+//             }
+
+//             return b[1] - a[1];
+//         });
+
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] % 2 == 0) {
+//                 hashMap.put(nums[i], hashMap.getOrDefault(nums[i], 0) + 1);
+//             }
+//         }
+
+//         for (int num : hashMap.keySet()) {
+//             pq.offer(new int[] {num, hashMap.get(num) });
+//         }
+
+//         if(pq.isEmpty())
+//         {
+//             return -1;
+//         }
+
+//         return pq.poll()[0];
+
+//     }
+// }
+
+                    //(2)
+
 class Solution {
     public int mostFrequentEven(int[] nums) {
-
-        int n = nums.length;
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (b[1] == a[1]) {
-                return a[0] - b[0];
-            }
-
-            return b[1] - a[1];
-        });
-
-        for (int i = 0; i < n; i++) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
-                hashMap.put(nums[i], hashMap.getOrDefault(nums[i], 0) + 1);
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
 
-        for (int num : hashMap.keySet()) {
-            pq.offer(new int[] {num, hashMap.get(num) });
+        int maxFreq = 0;
+        int ans = -1;
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (freq > maxFreq) {
+                maxFreq = freq;
+                ans = key;
+            } else if (freq == maxFreq && key < ans) {
+                ans = key;
+            }
         }
 
-        if(pq.isEmpty())
-        {
-            return -1;
-        }
-
-        return pq.poll()[0];
+        return ans;
 
     }
 }
