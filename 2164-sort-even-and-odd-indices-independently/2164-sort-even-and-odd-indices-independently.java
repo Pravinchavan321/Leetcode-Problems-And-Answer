b142d3b77d3cf508180f28d8abf6ae9c802859d4
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-10 17:18:11 +0530

    Time: 6 ms (53.58%), Space: 46.6 MB (38.52%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
@@ -1,39 +1,75 @@
+
+//(1)
+
+// class Solution {
+//     public int[] sortEvenOdd(int[] nums) {
+
+//         int n = nums.length;
+
+//         int[] even;
+//         if (n % 2 == 0) {
+
+//             even = new int[n / 2];
+//         } else {
+
+//             even = new int[n / 2 + 1];
+//         }
+
+//         int[] odd = new int[n / 2];
+
+//         int e = 0, o = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             if (i % 2 == 0)
+//                 even[e++] = nums[i];
+//             else
+//                 odd[o++] = nums[i];
+//         }
+
+//         Arrays.sort(even);
+//         Arrays.sort(odd);
+
+//         e = 0;
+//         o = odd.length - 1;
+
+//         for (int i = 0; i < n; i++) {
+//             if (i % 2 == 0)
+//                 nums[i] = even[e++];
+//             else
+//                 nums[i] = odd[o--];
+//         }
+
+//         return nums;
+//     }
+// }
+
+//(2)
+
 class Solution {
     public int[] sortEvenOdd(int[] nums) {
 
-        int n = nums.length;
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
 
-        int[] even;
-        if (n % 2 == 0) {
-
-            even = new int[n / 2];
-        } else {
-
-            even = new int[n / 2 + 1];
+        // Separate even and odd indexed elements
+        for (int i = 0; i < nums.length; i++) {
+            if ((i & 1) == 0)
+                even.add(nums[i]);
+            else
+                odd.add(nums[i]);
         }
 
-        int[] odd = new int[n / 2];
+        Collections.sort(even);
+
+        odd.sort(Collections.reverseOrder());
 
         int e = 0, o = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0)
-                even[e++] = nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            if ((i & 1) == 0)
+                nums[i] = even.get(e++);
             else
-                odd[o++] = nums[i];
-        }
-
-        Arrays.sort(even);
-        Arrays.sort(odd);
-
-        e = 0;
-        o = odd.length - 1;
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0)
-                nums[i] = even[e++];
-            else
-                nums[i] = odd[o--];
+                nums[i] = odd.get(o++);
         }
 
         return nums;
