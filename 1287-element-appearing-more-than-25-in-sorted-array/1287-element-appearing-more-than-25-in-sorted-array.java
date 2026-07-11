d315831b91ec386fb925aad6f7350e78d470ddc7
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-11 20:02:05 +0530

    Time: 6 ms (31.51%), Space: 47.2 MB (13.07%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -1,35 +1,60 @@
+//(1)
+
+// class Solution {
+//     public int findSpecialInteger(int[] arr) {
+
+//         int n = arr.length;
+//         int moreThan25 = n / 4;
+//         int i = 0;
+//         int j = 0;
+//         int ans = arr[i];
+//         while(j < n)
+//         {
+//             if(arr[i] == arr[j])
+//             {
+//                 if((j - i) + 1 > moreThan25)
+//                 {
+//                     ans = arr[i];
+//                     return ans;
+//                 }
+
+//                 j++;
+//             }
+//             else
+//             {
+//                 i = j;
+
+//             }
+
+//         }
+
+//         return ans;
+
+//     }
+// }
+
+//(2)
+
 class Solution {
     public int findSpecialInteger(int[] arr) {
 
         int n = arr.length;
         int moreThan25 = n / 4;
-        int i = 0;
-        int j = 0;
-        int ans = arr[i];
-        while(j < n)
-        {
-            if(arr[i] == arr[j])
-            {
-                if((j - i) + 1 > moreThan25)
-                {
-                    ans = arr[i];
-                    return ans;
-                }
-            
-                j++;
-            }
-            else
-            {
-                i = j;
-                
 
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int num : arr) {
+            int freq = hashMap.getOrDefault(num, 0) + 1;
+
+            if (freq > moreThan25) {
+                return num;
             }
 
-
+            hashMap.put(num, freq);
         }
 
-        return ans;
-
-        
+        return -1;
     }
 }
+
+//(3)
