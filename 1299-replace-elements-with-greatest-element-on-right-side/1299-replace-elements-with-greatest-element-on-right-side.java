a3a9e1686de30924bf9c2c48b7f9e89a012292eb
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-11 22:58:27 +0530

    Time: 3 ms (70.64%), Space: 48.8 MB (95.84%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,35 +1,38 @@
-// class Solution {
-//     public int[] replaceElements(int[] arr) {
-
-//         int n = arr.length;
-
-//         int prevElem = arr[n - 1];
-//         arr[n - 1] = -1;
-//         int max = Integer.MIN_VALUE;
-//         for (int i = n - 2; i >= 0; i--) {
-//             max = Math.max(prevElem, arr[i + 1]);
-//             prevElem = arr[i];
-//             arr[i] = max;
-
-//         }
-
-//         return arr;
-
-//     }
-// }
-
+//(1)
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int n = arr.length;
-        int maxRight = -1;
 
-        for(int i=n-1; i>=0; i--){
-            int current = arr[i];
-            arr[i] = maxRight;
-            if(current > maxRight){
-                maxRight = current;
-            }
+        int n = arr.length;
+
+        int prevElem = arr[n - 1];
+        arr[n - 1] = -1;
+        int max = Integer.MIN_VALUE;
+        for (int i = n - 2; i >= 0; i--) {
+            max = Math.max(prevElem, arr[i + 1]);
+            prevElem = arr[i];
+            arr[i] = max;
+
         }
+
         return arr;
+
     }
 }
+
+//(2)
+
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+//         int n = arr.length;
+//         int maxRight = -1;
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             int current = arr[i];
+//             arr[i] = maxRight;
+//             if (current > maxRight) {
+//                 maxRight = current;
+//             }
+//         }
+//         return arr;
+//     }
+// }
