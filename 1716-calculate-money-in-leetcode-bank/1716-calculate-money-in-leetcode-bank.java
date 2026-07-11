f0f1247939946b3b3bbf1a9903e7b72cbbf4a757
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-11 17:53:18 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.59%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,22 +1,47 @@
+
+                    //(1)
+
+
+// class Solution {
+//     public int totalMoney(int n) {
+
+//         int sum = 0;
+//         int remain = n;
+//         int tillElemSum = (n < 7) ? n : 7;
+//         int count = 0;
+
+//         while (remain > 0) {
+
+//             int days = Math.min(remain, 7);
+
+//             sum = sum + days * (2 * (count + 1) + (days - 1)) / 2;
+
+//             remain = remain - 7;
+//             count++;
+
+//         }
+
+//         return sum;
+//     }
+// }
+
+
+
+                        //(2)
+
+                        
 class Solution {
     public int totalMoney(int n) {
 
-        int sum = 0;
-        int remain = n;
-        int tillElemSum = (n < 7) ? n : 7;
-        int count = 0;
+        int weeks = n / 7;
+        int days = n % 7;
 
-        while (remain > 0) {
+        // Sum of complete weeks
+        int completeWeeks = weeks * 28 + (weeks * (weeks - 1) / 2) * 7;
 
-            int days = Math.min(remain, 7);
+        // Sum of remaining days
+        int remainingDays = days * (2 * (weeks + 1) + (days - 1)) / 2;
 
-            sum = sum + days * (2 * (count + 1) + (days - 1)) / 2;
-
-            remain = remain - 7;
-            count++;
-
-        }
-
-        return sum;
+        return completeWeeks + remainingDays;
     }
 }
