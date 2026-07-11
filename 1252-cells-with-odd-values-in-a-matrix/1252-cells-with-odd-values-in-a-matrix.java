618c558efa9d519c470142805a6ce3c502a8647d
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-11 22:22:02 +0530

    Time: 0 ms (100%), Space: 43.3 MB (66.34%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -1,35 +1,67 @@
+//(1)
+
+// class Solution {
+//     public int oddCells(int m, int n, int[][] indices) {
+
+//         int[][] arr = new int[m][n];
+//         int iL = indices.length;
+//         int odd = 0;
+//         for (int i = 0; i < iL; i++) {
+//             int row = indices[i][0];
+//             int col = indices[i][1];
+//             for (int i1 = 0; i1 < n; i1++) {
+//                 arr[row][i1] = arr[row][i1] + 1;
+
+//             }
+//             for (int i2 = 0; i2 < m; i2++) {
+//                 arr[i2][col] = arr[i2][col] + 1;
+
+//             }
+//             if (i == iL - 1) {
+//                 for (int i3 = 0; i3 < m; i3++) {
+//                     for (int j3 = 0; j3 < n; j3++) {
+//                         if (arr[i3][j3] % 2 != 0) {
+//                             odd++;
+//                         }
+
+//                     }
+//                 }
+
+//             }
+
+//         }
+
+//         return odd;
+
+//     }
+// }
+
+//(2)
+
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
+        int[] r = new int[m];
+        int[] c = new int[n];
 
-        int[][] arr = new int[m][n];
-        int iL = indices.length;
-        int odd = 0;
-        for (int i = 0; i < iL; i++) {
-            int row = indices[i][0];
-            int col = indices[i][1];
-            for (int i1 = 0; i1 < n; i1++) {
-                arr[row][i1] = arr[row][i1] + 1;
-
-            }
-            for (int i2 = 0; i2 < m; i2++) {
-                arr[i2][col] = arr[i2][col] + 1;
-
-            }
-            if (i == iL - 1) {
-                for (int i3 = 0; i3 < m; i3++) {
-                    for (int j3 = 0; j3 < n; j3++) {
-                        if (arr[i3][j3] % 2 != 0) {
-                            odd++;
-                        }
-
-                    }
-                }
-
-            }
-
+        for (int i = 0; i < indices.length; i++) {
+            r[indices[i][0]]++;
+            c[indices[i][1]]++;
         }
 
-        return odd;
+        int oddRows = 0;
+        for (int i = 0; i < m; i++) {
+            if (r[i] % 2 == 1) {
+                oddRows++;
+            }
+        }
 
+        int oddColumns = 0;
+        for (int i = 0; i < n; i++) {
+            if (c[i] % 2 == 1) {
+                oddColumns++;
+            }
+        }
+
+        return oddRows * n + oddColumns * m - 2 * oddRows * oddColumns;
     }
 }
