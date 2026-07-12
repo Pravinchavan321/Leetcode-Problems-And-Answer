4853c10784f39606953c1c9b05eccdba26ccab77
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-12 16:58:47 +0530

    Time: 34 ms (40.53%), Space: 77.1 MB (20.51%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -25,43 +25,63 @@
 // }
 
 //(2)
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+
+        int n = arr.length;
+        int[] arrCopy = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrCopy[i] = arr[i];
+
+        }
+        Arrays.sort(arr);
+
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int rank = 1;
+        for (int no : arr) {
+            if (!hashMap.containsKey(no)) {
+                hashMap.put(no, rank++);
+
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = hashMap.get(arrCopy[i]);
+        }
+
+        return arr;
+
+    }
+}
+
+//(3)
 // class Solution {
 //     public int[] arrayRankTransform(int[] arr) {
 
 //         int n = arr.length;
 
-//         return arr;
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
+//         for (int num : arr) {
+//             pq.offer(num);
+//         }
+
+//         HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+//         int rank = 1;
+
+//         while (!pq.isEmpty()) {
+//             int num = pq.poll();
+
+//             if (!hashMap.containsKey(num)) {
+//                 hashMap.put(num, rank++);
+//             }
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = hashMap.get(arr[i]);
+//         }
+
+//         return arr;
 //     }
 // }
-
-class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-
-        int n = arr.length;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int num : arr) {
-            pq.offer(num);
-        }
-
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        int rank = 1;
-
-        while (!pq.isEmpty()) {
-            int num = pq.poll();
-
-            if (!hashMap.containsKey(num)) {
-                hashMap.put(num, rank++);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = hashMap.get(arr[i]);
-        }
-
-        return arr;
-    }
-}
