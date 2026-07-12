fb0b287baa7dc803e4d10f6bc47c2661729b655f
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-12 16:48:26 +0530

    Time: 69 ms (5.93%), Space: 78.4 MB (9.09%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,23 +1,67 @@
+
+//(1)
+// class Solution {
+//     public int[] arrayRankTransform(int[] arr) {
+
+//         int n = arr.length;
+//         TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+//         for (int i = 0; i < n; i++) {
+//             treeMap.put(arr[i], 0);
+//         }
+
+//         int rank = 1;
+//         for (int num : treeMap.keySet()) {
+//             treeMap.put(num, rank++);
+
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = treeMap.get(arr[i]);
+//         }
+
+//         return arr;
+
+//     }
+// }
+
+//(2)
+// class Solution {
+//     public int[] arrayRankTransform(int[] arr) {
+
+//         int n = arr.length;
+
+//         return arr;
+
+//     }
+// }
+
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
 
         int n = arr.length;
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-        for (int i = 0; i < n; i++) {
-            treeMap.put(arr[i], 0);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int num : arr) {
+            pq.offer(num);
         }
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
         int rank = 1;
-        for (int num : treeMap.keySet()) {
-            treeMap.put(num, rank++);
 
+        while (!pq.isEmpty()) {
+            int num = pq.poll();
+
+            if (!hashMap.containsKey(num)) {
+                hashMap.put(num, rank++);
+            }
         }
 
         for (int i = 0; i < n; i++) {
-            arr[i] = treeMap.get(arr[i]);
+            arr[i] = hashMap.get(arr[i]);
         }
 
         return arr;
-
     }
 }
