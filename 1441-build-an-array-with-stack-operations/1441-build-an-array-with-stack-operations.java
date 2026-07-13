79d4ac6e28aa6e0c04b713c80feb8f73cdb5f998
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-13 18:57:08 +0530

    Time: 0 ms (100%), Space: 44.3 MB (52.62%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,50 +1,85 @@
+//(1)
+
+// class Solution {
+//     public List<String> buildArray(int[] target, int n) {
+
+//         Stack<Integer> st = new Stack<>();
+//         List<String> list = new ArrayList<>();
+//         int tL = target.length;
+//         int i = 0;
+//         int no = 1;
+
+//         if (target[tL - 1] > n) {
+//             return list;
+//         }
+//         if (target[tL - 1] == no) {
+//             list.add("Push");
+//             return list;
+//         }
+
+//         while (target[tL - 1] != no) {
+
+//             if (st.isEmpty()) {
+//                 st.push(no);
+//                 list.add("Push");
+//             } else if (!st.isEmpty() && target[i] == st.peek()) {
+//                 i++;
+//                 st.push(++no);
+//                 list.add("Push");
+//             } else if (!st.isEmpty() && target[i] != st.peek()) {
+//                 st.pop();
+//                 list.add("Pop");
+//                 list.add("Push");
+//                 st.push(++no);
+
+//             }
+
+//         }
+
+//         return list;
+
+//     }
+// }
+
+//(2)
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        
-        
-        Stack<Integer> st = new Stack<>();
+
         List<String> list = new ArrayList<>();
         int tL = target.length;
         int i = 0;
         int no = 1;
-
-        if(target[tL - 1] > n)
-        {
+        int prev = 1;;
+        if (target[tL - 1] > n) {
             return list;
         }
-        if(target[tL- 1] == no)
-        {
+        if (target[tL - 1] == no) {
             list.add("Push");
             return list;
         }
 
-        while(target[tL- 1] != no)
-        {
+        while (target[tL - 1] != no) {
 
-            if(st.isEmpty())
-            {
-                st.push(no);
+            if (list.size() == 0) {
+                prev = no;
                 list.add("Push");
-            }
-            else if(!st.isEmpty() && target[i] == st.peek())
-            {
+
+            } else if (list.size() != 0 && target[i] == prev) {
                 i++;
-                st.push(++no);
+                no++;
+                prev = no;
                 list.add("Push");
-            }
-            else if(!st.isEmpty() && target[i] != st.peek())
-            {
-                st.pop();
+            } else if (list.size() != 0 && target[i] != prev) {
+                no++;
+                prev = no;
                 list.add("Pop");
                 list.add("Push");
-                st.push(++no);
 
             }
 
         }
 
-
         return list;
-        
+
     }
 }
