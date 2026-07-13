d27e74c3e0511b65713e4955dcfbc0592274b200
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-13 15:01:00 +0530

    Time: 0 ms (100%), Space: 42.2 MB (87.35%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -56,24 +56,43 @@
 //     }
 // }
 
-
 //(2)
+
+// class Solution {
+//     public List<Integer> sequentialDigits(int low, int high) {
+//         List<Integer> list = new ArrayList<>();
+
+//         for (int len = 2; len <= 9; len++) {
+//             for (int start = 1; start <= 10 - len; start++) {
+
+//                 int num = 0;
+//                 int digit = start;
+
+//                 for (int i = 0; i < len; i++) {
+//                     num = num * 10 + digit;
+//                     digit++;
+//                 }
+
+//                 if (num >= low && num <= high) {
+//                     list.add(num);
+//                 }
+//             }
+//         }
+
+//         return list;
+//     }
+// }
+
+//(3)
 
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         List<Integer> list = new ArrayList<>();
+        String digits = "123456789";
 
         for (int len = 2; len <= 9; len++) {
-            for (int start = 1; start <= 10 - len; start++) {
-
-                int num = 0;
-                int digit = start;
-
-                for (int i = 0; i < len; i++) {
-                    num = num * 10 + digit;
-                    digit++;
-                }
-
+            for (int i = 0; i + len <= 9; i++) {
+                int num = Integer.parseInt(digits.substring(i, i + len));
                 if (num >= low && num <= high) {
                     list.add(num);
                 }
