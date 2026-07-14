30d40eedd25ea880795b0e19df52fe7c75182b84
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-14 22:43:32 +0530

    Time: 14 ms (89.01%), Space: 46.4 MB (92.44%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,11 +1,60 @@
+                    //(1)
+
+// class Solution {
+//     public int calculate(String s) {
+
+//         Stack<Integer> stack = new Stack<>();
+//         char sign = '+';
+//         int num = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+
+//             if (Character.isDigit(ch)) {
+//                 num = num * 10 + (ch - '0');
+//             }
+
+//             if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
+
+//                 if (sign == '+') {
+//                     stack.push(num);
+//                 } else if (sign == '-') {
+//                     stack.push(-num);
+//                 } else if (sign == '*') {
+//                     stack.push(stack.pop() * num);
+//                 } else if (sign == '/') {
+//                     stack.push(stack.pop() / num);
+//                 }
+
+//                 sign = ch;
+//                 num = 0;
+//             }
+//         }
+
+//         int ans = 0;
+//         while (!stack.isEmpty()) {
+//             ans += stack.pop();
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
+                        //(2)
+
+
 class Solution {
     public int calculate(String s) {
 
-        Stack<Integer> stack = new Stack<>();
-        char sign = '+';
+        int result = 0;
+        int lastNumber = 0;
         int num = 0;
+        char sign = '+';
 
         for (int i = 0; i < s.length(); i++) {
+
             char ch = s.charAt(i);
 
             if (Character.isDigit(ch)) {
@@ -15,13 +64,15 @@ class Solution {
             if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
 
                 if (sign == '+') {
-                    stack.push(num);
+                    result += lastNumber;
+                    lastNumber = num;
                 } else if (sign == '-') {
-                    stack.push(-num);
+                    result += lastNumber;
+                    lastNumber = -num;
                 } else if (sign == '*') {
-                    stack.push(stack.pop() * num);
+                    lastNumber = lastNumber * num;
                 } else if (sign == '/') {
-                    stack.push(stack.pop() / num);
+                    lastNumber = lastNumber / num;
                 }
 
                 sign = ch;
@@ -29,11 +80,6 @@ class Solution {
             }
         }
 
-        int ans = 0;
-        while (!stack.isEmpty()) {
-            ans += stack.pop();
-        }
-
-        return ans;
+        return result + lastNumber;
     }
 }
