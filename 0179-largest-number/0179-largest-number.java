225894aa4348095612011bec8e97eabba5259879
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-14 23:18:44 +0530

    Time: 6 ms (96.33%), Space: 44.7 MB (94.44%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,20 +1,48 @@
+//(1)
+
+// class Solution {
+//     public String largestNumber(int[] nums) {
+
+//         PriorityQueue<String> maxHeap = new PriorityQueue<>((a, b) -> (b + a).compareTo(a + b));
+
+//         for (int num : nums) {
+//             maxHeap.offer(String.valueOf(num));
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+
+//         while (!maxHeap.isEmpty()) {
+//             sb.append(maxHeap.poll());
+//         }
+
+//         if (sb.charAt(0) == '0') {
+//             return "0";
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+//(2)
 class Solution {
     public String largestNumber(int[] nums) {
 
-        PriorityQueue<String> maxHeap = new PriorityQueue<>((a, b) -> (b + a).compareTo(a + b));
+        String[] arr = new String[nums.length];
 
-        for (int num : nums) {
-            maxHeap.offer(String.valueOf(num));
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) {
+            return "0";
         }
 
         StringBuilder sb = new StringBuilder();
 
-        while (!maxHeap.isEmpty()) {
-            sb.append(maxHeap.poll());
-        }
-
-        if (sb.charAt(0) == '0') {
-            return "0";
+        for (String s : arr) {
+            sb.append(s);
         }
 
         return sb.toString();
