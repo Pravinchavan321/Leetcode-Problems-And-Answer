d8cae70beb162c5091dd38553033e91864eb8cf1
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-15 14:47:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (74.78%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,5 @@
 
-//(1)
+                //(1)
 
 // class Solution {
 
@@ -26,28 +26,38 @@
 //     }
 // }
 
-//(2)
+                    //(2)
 
+// class Solution {
+
+//     public int gcd(int a, int b) {
+//         if (b == 0) {
+//             return a;
+//         }
+//         return gcd(b, a % b);
+//     }
+
+//     public int gcdOfOddEvenSums(int n) {
+
+//         // int evenN = n * 2;
+//         // int oddN = (n * 2) - 1;
+//         // float evenSum = ((n / 2.0f) * (2 + evenN));
+//         // float oddSum = ((n / 2.0f) * (1 + oddN));
+
+//         float evenSum = (n / 2.0f) * ((2 * 2) + ((n - 1) * 2));
+//         float oddSum = (n / 2.0f) * ((2 * 1) + ((n - 1) * 2));
+
+//         return gcd((int) oddSum, (int) evenSum);
+
+//     }
+// }
+
+
+                    //(3)
 class Solution {
-
-    public int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
-
     public int gcdOfOddEvenSums(int n) {
-
-        // int evenN = n * 2;
-        // int oddN = (n * 2) - 1;
-        // float evenSum = ((n / 2.0f) * (2 + evenN));
-        // float oddSum = ((n / 2.0f) * (1 + oddN));
-
-        float evenSum = (n / 2.0f) * ((2 * 2) + ((n - 1) * 2));
-        float oddSum = (n / 2.0f) * ((2 * 1) + ((n - 1) * 2));
-
-        return gcd((int) oddSum, (int) evenSum);
-
+        int odd = n*n;
+        int even = n*(n+1);
+        return n;
     }
 }
