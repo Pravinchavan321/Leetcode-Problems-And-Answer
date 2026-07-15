90dc839a68cd295ef1b11b90bb72cda67de4b5ce
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-15 19:50:05 +0530

    Time: 0 ms (100%), Space: 42.8 MB (67.13%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -13,39 +13,65 @@
  *     }
  * }
  */
+
+//(1)
+// class Solution {
+
+//     public int sumNumbersAll(TreeNode root, int total, int num) {
+//         if (root == null) {
+//             return 0;
+//         }
+
+//         if (root.left == null && root.right == null) {
+//             num = num * 10 + root.val;
+//             total = total + num;
+//             return total;
+//         }
+
+//         num = num * 10 + root.val;
+//         if (root.left != null) {
+
+//             total = sumNumbersAll(root.left, total, num);
+//         }
+
+//         if (root.right != null) {
+//             total = sumNumbersAll(root.right, total, num);
+//         }
+
+//         return total;
+
+//     }
+
+//     public int sumNumbers(TreeNode root) {
+//         int num = 0;
+//         int total = 0;
+
+//         return sumNumbersAll(root, total, num);
+
+//     }
+
+// }
+
+//(2)
+
 class Solution {
 
-    public int sumNumbersAll(TreeNode root, int total, int num) {
+    public int sumNumbersAll(TreeNode root, int num) {
         if (root == null) {
             return 0;
         }
 
-        if (root.left == null && root.right == null) {
-            num = num * 10 + root.val;
-            total = total + num;
-            return total;
-        }
-
         num = num * 10 + root.val;
-        if (root.left != null) {
 
-            total = sumNumbersAll(root.left, total, num);
+        if (root.left == null && root.right == null) {
+            return num;
         }
 
-        if (root.right != null) {
-            total = sumNumbersAll(root.right, total, num);
-        }
-
-        return total;
-
+        return sumNumbersAll(root.left, num)
+                + sumNumbersAll(root.right, num);
     }
 
     public int sumNumbers(TreeNode root) {
-        int num = 0;
-        int total = 0;
-
-        return sumNumbersAll(root, total, num);
-
+        return sumNumbersAll(root, 0);
     }
-
 }
