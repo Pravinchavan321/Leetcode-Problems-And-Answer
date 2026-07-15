d1d695fca7caf5133407b28c763354fd4c6ee0bf
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-15 17:39:53 +0530

    Time: 0 ms (100%), Space: 46.3 MB (73.26%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,58 +1,58 @@
 
                     //(1)
 
-// class Solution {
+class Solution {
 
-//     public Node helper(Node root, Node parent, boolean isLeft) {
-//         if (root == null)
-//             return null;
+    public Node helper(Node root, Node parent, boolean isLeft) {
+        if (root == null)
+            return null;
 
-//         if (parent == null) {
-//             root.next = null;
-//         } else if (isLeft) {
-//             root.next = parent.right;
-//         } else {
-//             root.next = (parent.next == null) ? null : parent.next.left;
-//         }
+        if (parent == null) {
+            root.next = null;
+        } else if (isLeft) {
+            root.next = parent.right;
+        } else {
+            root.next = (parent.next == null) ? null : parent.next.left;
+        }
 
-//         helper(root.left, root, true);
-//         helper(root.right, root, false);
+        helper(root.left, root, true);
+        helper(root.right, root, false);
 
-//         return root;
-//     }
+        return root;
+    }
 
-//     public Node connect(Node root) {
-//         return helper(root, null, false);
-//     }
-// }
+    public Node connect(Node root) {
+        return helper(root, null, false);
+    }
+}
 
 
 
                 //(2)
 
 
-class Solution {
-    public Node connect(Node root) {
-        if (root == null) return null;
+// class Solution {
+//     public Node connect(Node root) {
+//         if (root == null) return null;
 
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(root);
+//         Queue<Node> q = new ArrayDeque<>();
+//         q.offer(root);
 
-        while (!q.isEmpty()) {
-            int size = q.size();
+//         while (!q.isEmpty()) {
+//             int size = q.size();
 
-            for (int i = 0; i < size; i++) {
-                Node curr = q.poll();
+//             for (int i = 0; i < size; i++) {
+//                 Node curr = q.poll();
 
-                if (i < size - 1) {
-                    curr.next = q.peek();
-                }
+//                 if (i < size - 1) {
+//                     curr.next = q.peek();
+//                 }
 
-                if (curr.left != null) q.offer(curr.left);
-                if (curr.right != null) q.offer(curr.right);
-            }
-        }
+//                 if (curr.left != null) q.offer(curr.left);
+//                 if (curr.right != null) q.offer(curr.right);
+//             }
+//         }
 
-        return root;
-    }
-}
+//         return root;
+//     }
+// }
