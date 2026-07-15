e11172fc02eacc47cfb7a905431ab1dc401976bc
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-15 14:43:27 +0530

    Time: 1 ms (83.11%), Space: 42.7 MB (20.94%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,3 +1,33 @@
+
+//(1)
+
+// class Solution {
+
+//     public int gcd(int a, int b) {
+//         if (b == 0) {
+//             return a;
+//         }
+//         return gcd(b, a % b);
+//     }
+
+//     public int gcdOfOddEvenSums(int n) {
+
+//         int evenSum = 0;
+//         int oddSum = 0;
+
+//         for (int i = 1; i <= (2 * n); i = i + 2) {
+//             oddSum = oddSum + i;
+//             evenSum = evenSum + (i + 1);
+
+//         }
+
+//         return gcd(oddSum, evenSum);
+
+//     }
+// }
+
+//(2)
+
 class Solution {
 
     public int gcd(int a, int b) {
@@ -8,17 +38,16 @@ class Solution {
     }
 
     public int gcdOfOddEvenSums(int n) {
+        int evenN = n * 2;
+        int oddN = (n * 2) - 1;
 
-        int evenSum = 0;
-        int oddSum = 0;
+        float evenSum = ((n / 2.0f) * (2 + evenN));
+        float oddSum = ((n / 2.0f) * (1 + oddN));
 
-        for (int i = 1; i <= (2 * n); i = i + 2) {
-            oddSum = oddSum + i;
-            evenSum = evenSum + (i + 1);
+        // int evenSum = (n / 2) * ((2 * 2) + ((n - 1) * 2));
+        // int oddSum = (n / 2) * ((2 * 1) + ((n - 1) * 2));
 
-        }
-
-        return gcd(oddSum, evenSum);
+        return gcd((int) oddSum, (int) evenSum);
 
     }
 }
