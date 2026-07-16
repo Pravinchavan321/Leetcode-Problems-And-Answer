0e4b11ea1261fba3aae181987049e875b078124e
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-16 15:22:21 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,60 @@
+//(1)
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         HashMap<Integer, Integer> count = new HashMap<>();
+//         count.put(0, 0);
+//         count.put(1, 0);
+//         count.put(2, 0);
+
+//         for (int num : nums) {
+//             count.put(num, count.get(num) + 1);
+//         }
+
+//         int idx = 0;
+//         for (int color = 0; color < 3; color++) {
+//             int freq = count.get(color);
+//             for (int j = 0; j < freq; j++) {
+//                 nums[idx] = color;
+//                 idx++;
+//             }
+//         }
+//     }
+// }
+
+//(2)
+
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        count.put(0, 0);
-        count.put(1, 0);
-        count.put(2, 0);
+        int n = nums.length;
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
 
         for (int num : nums) {
-            count.put(num, count.get(num) + 1);
+            if (num == 0) {
+                countZero++;
+            } else if (num == 1) {
+                countOne++;
+
+            } else {
+                countTwo++;
+            }
         }
 
-        int idx = 0;
-        for (int color = 0; color < 3; color++) {
-            int freq = count.get(color);
-            for (int j = 0; j < freq; j++) {
-                nums[idx] = color;
-                idx++;
+        for (int i = 0; i < n; i++) {
+            if (countZero != 0) {
+                nums[i] = 0;
+                countZero--;
+            } else if (countOne != 0) {
+                nums[i] = 1;
+                countOne--;
+            } else {
+                nums[i] = 2;
+                countTwo--;
             }
-        }        
+
+        }
+
     }
 }
