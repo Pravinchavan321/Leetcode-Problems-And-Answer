795acd36bd92435002be853053595ed7f3a8471f
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-16 14:26:36 +0530

    Time: 51 ms (100%), Space: 108.1 MB (53%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -1,26 +1,71 @@
-class Solution {
-    public long gcd(long a, long b) {
-        if (b == 0) {
-            return a;
-        }
 
-        return gcd(b, a % b);
+//(1)
+
+// class Solution {
+//     public int gcd(int a, int b) {
+//         if (b == 0) {
+//             return a;
+//         }
+
+//         return gcd(b, a % b);
+//     }
+
+//     public long gcdSum(int[] nums) {
+
+//         int n = nums.length;
+
+//         int[] prefixGcd = new int[n];
+
+//         int mx = Integer.MIN_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             if(nums[i] > mx)
+//             {
+//                 mx = nums[i];
+
+//             }
+
+//             prefixGcd[i] = gcd(mx, nums[i]);
+
+//         }
+//         Arrays.sort(prefixGcd);
+
+//         int i = 0;
+//         int j = n - 1;
+//         long sum = 0;
+//         while (i < j) {
+//             sum = sum + gcd(prefixGcd[i++], prefixGcd[j--]);
+
+//         }
+
+//         return sum;
+
+//     }
+// }
+
+//(2)
+
+class Solution {
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
     public long gcdSum(int[] nums) {
 
         int n = nums.length;
 
-        long[] prefixGcd = new long[n];
+        int[] prefixGcd = new int[n];
 
-        long mx = Integer.MIN_VALUE;
+        int mx = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            if(nums[i] > mx)
-            {
+            if (nums[i] > mx) {
                 mx = nums[i];
 
             }
-            
 
             prefixGcd[i] = gcd(mx, nums[i]);
 
