b197adc038919016c75a24f41abafe6e1b99fddc
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-17 18:11:32 +0530

    Time: 0 ms (100%), Space: 46.8 MB (53.52%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -0,0 +1,109 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//  (((This is to find minDiff between the in tree between root & left and root & right)))
+// class Solution {
+
+//     public int getMinimumDifference(TreeNode root) {
+
+//         int minDiff = Integer.MAX_VALUE;
+
+//         if (root.left != null) {
+
+//             minDiff = Math.min(Math.abs(root.val - root.left.val), getMinimumDifference(root.left));
+//         }
+//         if (root.right != null) {
+//             minDiff = Math.min(minDiff,
+//                     Math.min(Math.abs(root.val - root.right.val), getMinimumDifference(root.right)));
+
+//         }
+
+//         return minDiff;
+
+//     }
+// }
+
+//(1)
+
+// class Solution {
+
+//     public List<Integer> list = new ArrayList<>();
+
+//     public void inorderBst(TreeNode root) {
+
+//         if (root == null) {
+//             return;
+//         }
+//         inorderBst(root.left);
+//         list.add(root.val);
+//         inorderBst(root.right);
+
+//         return;
+
+//     }
+
+//     public int getMinimumDifference(TreeNode root) {
+//         inorderBst(root);
+
+//         int n = list.size();
+//         int minDiff = Integer.MAX_VALUE;
+//         for (int i = 0; i < n - 1; i++) {
+//             int diff = Math.abs(list.get(i) - list.get(i + 1));
+//             if (diff < minDiff) {
+//                 minDiff = diff;
+//             }
+
+//         }
+
+//         return minDiff;
+
+//     }
+// }
+
+//(2)
+
+class Solution {
+
+    public int previous = -1;
+    public int minDiff = Integer.MAX_VALUE;
+
+    public void inorderBst(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+        inorderBst(root.left);
+        if (previous == -1) {
+
+            previous = root.val;
+        } else if (minDiff > Math.abs(root.val - previous)) {
+
+            minDiff = Math.abs(root.val - previous);
+        }
+        previous = root.val;
+        inorderBst(root.right);
+
+        return;
+
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        inorderBst(root);
+
+        return minDiff;
+
+    }
+}
