3f87b42ccc3e972564dc5402c0ddb6b4b56280ed
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-18 02:02:22 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,42 @@
+
+//(1)
+
+// class Solution {
+//     public int fib(int n) {
+//         if (n == 0) {
+//             return 0;
+
+//         }
+//         if (n == 1) {
+//             return 1;
+//         }
+
+//         return fib(n - 1) + fib(n - 2);
+
+//     }
+// }
+
 class Solution {
     public int fib(int n) {
+
+        int secondLast = 0;
+        int firstLast = 1;
+
         if (n == 0) {
             return 0;
-
         }
         if (n == 1) {
             return 1;
         }
+        int i = 2;
+        while (i <= n) {
+            int temp = secondLast;
+            secondLast = firstLast;
+            firstLast = firstLast + temp;
+            i++;
+        }
 
-        return fib(n - 1) + fib(n - 2);
+        return firstLast;
 
     }
 }
