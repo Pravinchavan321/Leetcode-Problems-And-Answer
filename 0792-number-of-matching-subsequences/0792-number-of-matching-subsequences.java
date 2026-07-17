41a88be50c611bb4cbef43c0f27dec6c196c01dc
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-17 19:59:37 +0530

    Time: 43 ms (86.53%), Space: 49.1 MB (62.58%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
@@ -1,66 +1,123 @@
+//(1)
+
+// class Solution {
+//     public int numMatchingSubseq(String s, String[] words) {
+
+//         List<Integer>[] index = new ArrayList[26];
+
+//         for (int i = 0; i < 26; i++) {
+//             index[i] = new ArrayList<>();
+//         }
+
+//         // Store indices of each character
+//         for (int i = 0; i < s.length(); i++) {
+//             index[s.charAt(i) - 'a'].add(i);
+//         }
+
+//         int count = 0;
+
+//         for (String word : words) {
+
+//             int prev = -1;
+//             boolean found = true;
+
+//             for (char ch : word.toCharArray()) {
+
+//                 List<Integer> list = index[ch - 'a'];
+
+//                 int pos = upperBound(list, prev);
+
+//                 if (pos == list.size()) {
+//                     found = false;
+//                     break;
+//                 }
+
+//                 prev = list.get(pos);
+//             }
+
+//             if (found)
+//                 count++;
+//         }
+
+//         return count;
+//     }
+
+//     // First index whose value > target
+//     private int upperBound(List<Integer> list, int target) {
+
+//         int low = 0;
+//         int high = list.size() - 1;
+//         int ans = list.size();
+
+//         while (low <= high) {
+
+//             int mid = low + (high - low) / 2;
+
+//             if (list.get(mid) > target) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+//(2)
+
 import java.util.*;
 
 class Solution {
+
+    class Node {
+        String word;
+        int index;
+
+        Node(String word, int index) {
+            this.word = word;
+            this.index = index;
+        }
+    }
+
     public int numMatchingSubseq(String s, String[] words) {
 
-        List<Integer>[] index = new ArrayList[26];
+        List<Node>[] buckets = new ArrayList[26];
 
         for (int i = 0; i < 26; i++) {
-            index[i] = new ArrayList<>();
+            buckets[i] = new ArrayList<>();
         }
 
-        // Store indices of each character
-        for (int i = 0; i < s.length(); i++) {
-            index[s.charAt(i) - 'a'].add(i);
+        // Put every word into the bucket of its first character
+        for (String word : words) {
+            buckets[word.charAt(0) - 'a'].add(new Node(word, 0));
         }
 
         int count = 0;
 
-        for (String word : words) {
+        for (char ch : s.toCharArray()) {
 
-            int prev = -1;
-            boolean found = true;
+            List<Node> current = buckets[ch - 'a'];
 
-            for (char ch : word.toCharArray()) {
+            // Create a new empty bucket for this character
+            buckets[ch - 'a'] = new ArrayList<>();
 
-                List<Integer> list = index[ch - 'a'];
+            for (Node node : current) {
 
-                int pos = upperBound(list, prev);
+                node.index++;
 
-                if (pos == list.size()) {
-                    found = false;
-                    break;
+                if (node.index == node.word.length()) {
+                    count++;
+                } else {
+
+                    char next = node.word.charAt(node.index);
+
+                    buckets[next - 'a'].add(node);
                 }
-
-                prev = list.get(pos);
             }
-
-            if (found)
-                count++;
         }
 
         return count;
-    }
-
-    // First index whose value > target
-    private int upperBound(List<Integer> list, int target) {
-
-        int low = 0;
-        int high = list.size() - 1;
-        int ans = list.size();
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (list.get(mid) > target) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return ans;
     }
 }
