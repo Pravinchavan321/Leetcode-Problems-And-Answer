ca9333ef5b0e51a0a8baf37183ec9cc21dbad95b
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-17 19:12:04 +0530

    Time: 4 ms (83.55%), Space: 42.3 MB (42.2%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,22 +1,65 @@
+//(1)
+
+
+// class Solution {
+//     public int rotatedDigits(int n) {
+//         int count = 0;
+//         for (int i = 1; i <= n; i++) {
+//             String numStr = String.valueOf(i);
+//             if (numStr.indexOf("3") == -1 &&
+//                     numStr.indexOf("4") == -1 &&
+//                     numStr.indexOf("7") == -1 &&
+//                     (numStr.indexOf("2") != -1 ||
+//                             numStr.indexOf("5") != -1 ||
+//                             numStr.indexOf("6") != -1 ||
+//                             numStr.indexOf("9") != -1)) {
+
+//                 count++;
+//             }
+
+//         }
+
+//         return count;
+
+//     }
+// }
+
+
+
+//(2)
+
 class Solution {
     public int rotatedDigits(int n) {
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            String numStr = String.valueOf(i);
-            if (numStr.indexOf("3") == -1 &&
-                    numStr.indexOf("4") == -1 &&
-                    numStr.indexOf("7") == -1 &&
-                    (numStr.indexOf("2") != -1 ||
-                            numStr.indexOf("5") != -1 ||
-                            numStr.indexOf("6") != -1 ||
-                            numStr.indexOf("9") != -1)) {
 
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) {
                 count++;
             }
-
         }
 
         return count;
+    }
 
+    private boolean isGood(int num) {
+
+        boolean changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit == 3 || digit == 4 || digit == 7) {
+                return false;
+            }
+
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                changed = true;
+            }
+
+            num /= 10;
+        }
+
+        return changed;
     }
 }
