07eaac4be66988751780c9be9406fb767c40a0b2
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-17 18:12:07 +0530

    Time: 0 ms (100%), Space: 43.3 MB (12.57%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.java
@@ -13,9 +13,49 @@
  *     }
  * }
  */
+//(1)
+
+// class Solution {
+
+//     public List<Integer> list = new ArrayList<>();
+
+//     public void inorderBst(TreeNode root) {
+
+//         if (root == null) {
+//             return;
+//         }
+//         inorderBst(root.left);
+//         list.add(root.val);
+//         inorderBst(root.right);
+
+//         return;
+
+//     }
+
+//     public int minDiffInBST(TreeNode root) {
+//         inorderBst(root);
+
+//         int n = list.size();
+//         int minDiff = Integer.MAX_VALUE;
+//         for (int i = 0; i < n - 1; i++) {
+//             int diff = Math.abs(list.get(i) - list.get(i + 1));
+//             if (diff < minDiff) {
+//                 minDiff = diff;
+//             }
+
+//         }
+
+//         return minDiff;
+
+//     }
+// }
+
+//(2)
+
 class Solution {
 
-    public List<Integer> list = new ArrayList<>();
+    public int previous = -1;
+    public int minDiff = Integer.MAX_VALUE;
 
     public void inorderBst(TreeNode root) {
 
@@ -23,7 +63,14 @@ class Solution {
             return;
         }
         inorderBst(root.left);
-        list.add(root.val);
+        if (previous == -1) {
+
+            previous = root.val;
+        } else if (minDiff > Math.abs(root.val - previous)) {
+
+            minDiff = Math.abs(root.val - previous);
+        }
+        previous = root.val;
         inorderBst(root.right);
 
         return;
@@ -32,16 +79,6 @@ class Solution {
 
     public int minDiffInBST(TreeNode root) {
         inorderBst(root);
-
-        int n = list.size();
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 0; i < n - 1; i++) {
-            int diff = Math.abs(list.get(i) - list.get(i + 1));
-            if (diff < minDiff) {
-                minDiff = diff;
-            }
-
-        }
 
         return minDiff;
 
