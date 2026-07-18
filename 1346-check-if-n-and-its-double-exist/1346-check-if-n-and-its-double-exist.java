21f2a28070dfd14c84773696f0eb0fc1b0faa496
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-19 04:41:59 +0530

    Time: 2 ms (97.67%), Space: 45.2 MB (13.39%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,12 +1,9 @@
-class Solution 
-{
-    public boolean checkIfExist(int[] arr) 
-    {
+//(1)
+class Solution {
+    public boolean checkIfExist(int[] arr) {
         Set<Integer> set = new HashSet<>();
-        for (int i : arr) 
-        {
-            if (set.contains(i * 2) || (i % 2 == 0 && set.contains(i / 2))) 
-            {
+        for (int i : arr) {
+            if (set.contains(i * 2) || (i % 2 == 0 && set.contains(i / 2))) {
                 return true;
             }
             set.add(i);
@@ -16,3 +13,31 @@ class Solution
     }
 }
 
+//(2)
+
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         Set<Integer> set = new HashSet<>();
+//         int countZero = 0;
+//         for (int i : arr) {
+
+//             set.add(i);
+//             if (i == 0 && countZero < 2) {
+//                 countZero++;
+//             }
+
+//         }
+//         for (int i : arr) {
+//             if (i != 0) {
+//                 if (set.contains(i * 2)) {
+//                     return true;
+//                 }
+//             } else if (i == 0 && countZero > 1) {
+//                 return true;
+//             }
+
+//         }
+
+//         return false;
+//     }
+// }
