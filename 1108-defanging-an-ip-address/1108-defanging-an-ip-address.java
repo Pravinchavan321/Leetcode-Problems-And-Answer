86c6f6b2989846d4342abc3fe06faae0e774035f
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-18 21:54:11 +0530

    Time: 0 ms (100%), Space: 42.6 MB (78.49%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,23 +1,52 @@
+
+//(1)
+// class Solution {
+//     public String defangIPaddr(String address) {
+
+//         String[] arr = address.split("\\.");
+//         int n = arr.length;
+//         String[] res = new String[7];
+//         int j = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (i != n - 1) {
+//                 res[j++] = arr[i];
+//                 res[j++] = "[.]";
+
+//             } else {
+//                 res[j] = arr[i];
+
+//             }
+
+//         }
+
+//         return String.join("", res);
+
+//     }
+// }
+
+//(2)
 class Solution {
     public String defangIPaddr(String address) {
 
-        String[] arr = address.split("\\.");
-        int n = arr.length;
-        String[] res = new String[7];
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (i != n - 1) {
-                res[j++] = arr[i];
-                res[j++] = "[.]";
+        StringBuilder sb = new StringBuilder();
+        int n = address.length();
+
+        int i = 0;
+
+        while (i < n) {
+            char c = address.charAt(i);
+            if (c != '.') {
+                sb.append(c);
 
             } else {
-                res[j] = arr[i];
+                sb.append("[.]");
 
             }
+            i++;
 
         }
 
-        return String.join("", res);
+        return sb.toString();
 
     }
 }
