5a9476ef8b797ec8a1867f7984b8ac4afb14ba23
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-18 20:46:49 +0530

    Time: 11 ms (90.55%), Space: 47.5 MB (16.07%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,32 +1,55 @@
+//(1)
+
+// class Solution {
+//     public String removeDuplicates(String s) {
+
+//         StringBuilder sb = new StringBuilder();
+
+//         sb.append(s.charAt(0));
+//         int i = 0;
+//         int j = 1;
+//         int n = s.length();
+
+//         while (j < n) {
+//             if (sb.length() != 0 && sb.charAt(i) == s.charAt(j)) {
+//                 sb.deleteCharAt(sb.length() - 1);
+
+//                 i--;
+//                 j++;
+//             } else if (sb.length() == 0) {
+//                 sb.append(s.charAt(j));
+//                 i = 0;
+//                 j++;
+//             } else {
+//                 sb.append(s.charAt(j));
+//                 i++;
+//                 j++;
+//             }
+
+//         }
+
+//         return sb.toString();
+
+//     }
+// }
+
+//(2)
+
 class Solution {
     public String removeDuplicates(String s) {
-
         StringBuilder sb = new StringBuilder();
 
-        sb.append(s.charAt(0));
-        int i = 0;
-        int j = 1;
-        int n = s.length();
+        for (char c : s.toCharArray()) {
+            int len = sb.length();
 
-        while (j < n) {
-            if (sb.length() != 0 && sb.charAt(i) == s.charAt(j)) {
-                sb.deleteCharAt(sb.length() - 1);
-
-                i--;
-                j++;
-            } else if (sb.length() == 0) {
-                sb.append(s.charAt(j));
-                i = 0;
-                j++;
+            // If the StringBuilder isn't empty and the last char matches the current char
+            if (len > 0 && sb.charAt(len - 1) == c) {
+                sb.deleteCharAt(len - 1); // "Pop" the duplicate
             } else {
-                sb.append(s.charAt(j));
-                i++;
-                j++;
+                sb.append(c); // "Push" the new character
             }
-
         }
 
         return sb.toString();
-
     }
 }
