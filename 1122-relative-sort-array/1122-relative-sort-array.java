8bad39e41e06c3781b222b823b09effb5cb374ab
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-18 22:29:24 +0530

    Time: 3 ms (47.13%), Space: 44.3 MB (8.14%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,40 +1,87 @@
+
+//(1)
+
+// class Solution {
+//     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+//         Set<Integer> hashSet = new HashSet<>();
+//         Map<Integer, Integer> hashMap = new HashMap<>();
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+//         for (int num : arr2) {
+//             hashSet.add(num);
+//         }
+//         for (int num : arr1) {
+//             hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+//             if (!hashSet.contains(num)) {
+//                 hashSet.add(num);
+//                 minHeap.offer(num);
+
+//             }
+
+//         }
+//         int idx = 0;
+//         for (int i = 0; i < arr2.length; i++) {
+//             int count = hashMap.get(arr2[i]);
+//             while (count > 0) {
+//                 arr1[idx++] = arr2[i];
+//                 count--;
+//             }
+//         }
+//         while (!minHeap.isEmpty()) {
+//             int num = minHeap.poll();
+//             int count = hashMap.get(num);
+//             while (count > 0) {
+//                 arr1[idx++] = num;
+//                 count--;
+//             }
+//         }
+
+//         return arr1;
+
+//     }
+// }
+
+//(2)
+
+import java.util.TreeMap;
+
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        // TreeMap keeps the keys (numbers) automatically sorted!
+        TreeMap<Integer, Integer> map = new TreeMap<>();
 
-        Set<Integer> hashSet = new HashSet<>();
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        for (int num : arr2) {
-            hashSet.add(num);
-        }
+        // 1. Count frequencies of all elements in arr1
         for (int num : arr1) {
-            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
-            if (!hashSet.contains(num)) {
-                hashSet.add(num);
-                minHeap.offer(num);
-
-            }
-
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+
+        int[] res = new int[arr1.length];
         int idx = 0;
-        for (int i = 0; i < arr2.length; i++) {
-            int count = hashMap.get(arr2[i]);
-            while (count > 0) {
-                arr1[idx++] = arr2[i];
-                count--;
+
+        // 2. Place elements based on the order in arr2
+        for (int num : arr2) {
+            if (map.containsKey(num)) {
+                int count = map.get(num);
+                while (count > 0) {
+                    res[idx++] = num;
+                    count--;
+                }
+                // Remove the element so we only have "leftovers" remaining
+                map.remove(num);
             }
         }
-        while (!minHeap.isEmpty()) {
-            int num = minHeap.poll();
-            int count = hashMap.get(num);
+
+        // 3. Place remaining elements. 
+        // Because it's a TreeMap, map.keySet() is already in ascending order!
+        for (int num : map.keySet()) {
+            int count = map.get(num);
             while (count > 0) {
-                arr1[idx++] = num;
+                res[idx++] = num;
                 count--;
             }
         }
 
-        return arr1;
-
+        return res;
     }
 }
