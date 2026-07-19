babbab1d5f7ccbd797e3f14090724e50ec482868
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-19 06:37:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (44.13%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,18 +1,36 @@
+//(1)
+
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-
-        int n = index.length;
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = n - 2; j >= index[i]; j--) {
-                res[j + 1] = res[j];
-
-            }
-            res[index[i]] = nums[i];
-
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
         }
-
-        return res;
-
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
     }
 }
+
+//(2)
+
+// class Solution {
+//     public int[] createTargetArray(int[] nums, int[] index) {
+
+//         int n = index.length;
+//         int[] res = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             for (int j = n - 2; j >= index[i]; j--) {
+//                 res[j + 1] = res[j];
+
+//             }
+//             res[index[i]] = nums[i];
+
+//         }
+
+//         return res;
+
+//     }
+// }
