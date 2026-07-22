1c908b7b722b49ffb73463aa072863ddea91edac
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-22 15:28:35 +0530

    Time: 5 ms (44.6%), Space: 46.6 MB (30.66%) - LeetHub

---
+++ 1417-reformat-the-string/1417-reformat-the-string.java
@@ -0,0 +1,98 @@
+//(1)
+
+// class Solution {
+//     public String reformat(String s) {
+
+//         int n = s.length();
+
+//         int countLetter = 0;
+//         int countDigit = 0;
+//         List<Character> listLetter = new ArrayList<>();
+//         List<Character> listDigit = new ArrayList<>();
+
+//         for (int i = 0; i < n; i++) {
+//             char c = s.charAt(i);
+//             if (Character.isLetter(c)) {
+//                 countLetter++;
+//                 listLetter.add(c);
+//             } else {
+//                 countDigit++;
+//                 listDigit.add(c);
+//             }
+//         }
+
+//         if ((countLetter == 0 || countDigit == 0) && n > 1) {
+//             return "";
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+//         int iL = 0;
+//         int jD = 0;
+//         int idx = 0;
+//         if (countLetter == countDigit || countLetter - 1 == countDigit || countLetter == countDigit - 1) {
+//             if()
+//             while (idx < n) {
+//                 if (idx % 2 == 0) {
+//                     if (countLetter >= countDigit) {
+
+//                         sb.append(listLetter.get(iL++));
+//                     } else {
+//                         sb.append(listDigit.get(jD++));
+
+//                     }
+
+//                 } else {
+//                     if (countLetter < countDigit) {
+
+//                         sb.append(listLetter.get(iL++));
+//                     } else {
+//                         sb.append(listDigit.get(jD++));
+
+//                     }
+
+//                 }
+//                 idx++;
+//             }
+
+//         } else {
+//             return "";
+//         }
+
+//         return sb.toString();
+
+//     }
+// }
+
+//(2)
+
+class Solution {
+    public String reformat(String s) {
+        List<Character> letters = new ArrayList<>();
+        List<Character> digits = new ArrayList<>();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c)) letters.add(c);
+            else digits.add(c);
+        }
+
+        int lCount = letters.size(), dCount = digits.size();
+
+        if (Math.abs(lCount - dCount) > 1) return "";
+
+        StringBuilder sb = new StringBuilder();
+        int iL = 0, iD = 0;
+
+        // Jo zyada hai wo pehle aayega (even index pe)
+        boolean letterFirst = lCount >= dCount;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                sb.append(letterFirst ? letters.get(iL++) : digits.get(iD++));
+            } else {
+                sb.append(letterFirst ? digits.get(iD++) : letters.get(iL++));
+            }
+        }
+
+        return sb.toString();
+    }
+}
