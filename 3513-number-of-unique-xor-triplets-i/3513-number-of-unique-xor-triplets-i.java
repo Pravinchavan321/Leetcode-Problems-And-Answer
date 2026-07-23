6023f41838cd676351bdd0f6958f3f52c7d886b9
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-23 20:32:28 +0530

    Time: 0 ms (100%), Space: 125.3 MB (53.06%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,14 +1,26 @@
+//(1)
+
+// class Solution {
+//     public int uniqueXorTriplets(int[] nums) {
+//         int n = nums.length;
+
+//         if (n <= 2) return n;
+
+//         int mask = 0;
+//         for (int num : nums) {
+//             mask |= num;
+//         }
+
+//         return mask + 1;
+//     }
+// }
+
+//(2)
+
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
 
-        if (n <= 2) return n;
-
-        int mask = 0;
-        for (int num : nums) {
-            mask |= num;
-        }
-
-        return mask + 1;
+        return 1 << (32 - Integer.numberOfLeadingZeros(n) - 3 / (n + 1));
     }
 }
