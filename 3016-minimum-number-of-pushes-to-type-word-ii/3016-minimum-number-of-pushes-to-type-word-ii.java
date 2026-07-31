267e2a902435271dc58e8dc0c848314d070564f6
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-07-31 14:42:49 +0530

    Time: 10 ms (66.76%), Space: 48 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,43 +1,71 @@
+//(1)
+// class Solution {
+//     public int minimumPushes(String word) {
+
+//         int pushes = 0;
+
+//         int n = word.length();
+ 
+//         Integer[] freqArr = new Integer[26];
+//         Arrays.fill(freqArr, 0);
+//         for (int i = 0; i < n; i++) {
+//             freqArr[word.charAt(i) - 'a']++;
+//         }
+
+//         Arrays.sort(freqArr, Collections.reverseOrder());
+
+//         int count = 0;
+//         for (int i = 0; i < 26; i++) {
+//             if (freqArr[i] == 0) {
+//                 continue;
+//             }
+//             if (count < 8) {
+//                 count++;
+//                 pushes += freqArr[i];
+
+//             } else if (count >= 8 && count < 16) {
+//                 count++;
+//                 pushes += 2 * freqArr[i];
+
+//             } else if (count >= 16 && count < 24) {
+//                 count++;
+//                 pushes += 3 * freqArr[i];
+
+//             } else {
+//                 count++;
+//                 pushes += 4 * freqArr[i];
+
+//             }
+//         }
+
+//         return pushes;
+
+//     }
+// }
+
+
+
+//(2)
 class Solution {
     public int minimumPushes(String word) {
 
-        int pushes = 0;
-
-        int n = word.length();
- 
-        Integer[] freqArr = new Integer[26];
-        Arrays.fill(freqArr, 0);
-        for (int i = 0; i < n; i++) {
-            freqArr[word.charAt(i) - 'a']++;
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
         }
 
-        Arrays.sort(freqArr, Collections.reverseOrder());
+        Arrays.sort(freq);  // ascending
 
+        int pushes = 0;
         int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (freqArr[i] == 0) {
-                continue;
-            }
-            if (count < 8) {
-                count++;
-                pushes += freqArr[i];
 
-            } else if (count >= 8 && count < 16) {
-                count++;
-                pushes += 2 * freqArr[i];
-
-            } else if (count >= 16 && count < 24) {
-                count++;
-                pushes += 3 * freqArr[i];
-
-            } else {
-                count++;
-                pushes += 4 * freqArr[i];
-
-            }
+        // reverse iterate karo (highest freq pehle)
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+            pushes += freq[i] * (count / 8 + 1);
+            count++;
         }
 
         return pushes;
-
     }
 }
