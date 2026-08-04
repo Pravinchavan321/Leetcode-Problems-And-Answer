a3ca109062042043f75f771e6755cd74548f92d4
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-08-04 13:12:59 +0530

    Time: 3 ms (81.76%), Space: 46.8 MB (53.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,11 +1,49 @@
+//(1)
+
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+
+//         List<Integer> list = new ArrayList<>();
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         int min = nums[0];
+//         int max = nums[n - 1];
+//         Set<Integer> hashSet = new HashSet<>();
+
+//         for (int i = 0; i < n; i++) {
+//             hashSet.add(nums[i]);
+//         }
+
+//         for (int i = min; i <= max; i++) {
+//             if (!hashSet.contains(i)) {
+//                 list.add(i);
+//             }
+//         }
+
+//         return list;
+
+//     }
+// }
+
+//(2)
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
 
         List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
+
         int n = nums.length;
-        int min = nums[0];
-        int max = nums[n - 1];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+
+            }
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
         Set<Integer> hashSet = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
