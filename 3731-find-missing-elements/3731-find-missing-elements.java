5879075d23c4d1b1f97a3b9053a99ed34320eb1a
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-08-04 13:31:09 +0530

    Time: 1 ms (100%), Space: 46.5 MB (91.28%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -27,6 +27,42 @@
 
 //(2)
 
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+
+//         List<Integer> list = new ArrayList<>();
+
+//         int n = nums.length;
+//         int min = Integer.MAX_VALUE;
+//         int max = Integer.MIN_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] < min) {
+//                 min = nums[i];
+
+//             }
+//             if (nums[i] > max) {
+//                 max = nums[i];
+//             }
+//         }
+//         Set<Integer> hashSet = new HashSet<>();
+
+//         for (int i = 0; i < n; i++) {
+//             hashSet.add(nums[i]);
+//         }
+
+//         for (int i = min; i <= max; i++) {
+//             if (!hashSet.contains(i)) {
+//                 list.add(i);
+//             }
+//         }
+
+//         return list;
+
+//     }
+// }
+
+//(3)
+
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
 
@@ -38,20 +74,20 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (nums[i] < min) {
                 min = nums[i];
-
             }
+
             if (nums[i] > max) {
                 max = nums[i];
             }
         }
-        Set<Integer> hashSet = new HashSet<>();
+        int[] isPresentArr = new int[max + 1];
 
         for (int i = 0; i < n; i++) {
-            hashSet.add(nums[i]);
+            isPresentArr[nums[i]] = 1;
         }
 
         for (int i = min; i <= max; i++) {
-            if (!hashSet.contains(i)) {
+            if (isPresentArr[i] == 0) {
                 list.add(i);
             }
         }
