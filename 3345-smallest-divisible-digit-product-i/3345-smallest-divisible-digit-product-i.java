eb8010d39eb74d94cbdfe48a71291cbd9f5b74b1
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-08-06 20:55:50 +0530

    Time: 1 ms (100%), Space: 43 MB (14.66%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+        int currNum = n;
+        int res = n;
+        while (true) {
+            int currNumCopy = currNum;
+            int product = 1;
+            while (currNum > 0) {
+                int digit = currNum % 10;
+                product *= digit;
+                currNum /= 10;
+
+            }
+            if (product % t == 0) {
+                res = currNumCopy;
+                break;
+
+            }
+            currNum = currNumCopy + 1;
+
+        }
+
+        return res;
+
+    }
+}
