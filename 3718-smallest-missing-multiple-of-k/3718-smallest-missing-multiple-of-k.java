20ee5e7ab64d7166e2d46466009f24ce1d584fa1
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-08-26 03:24:33 +0530

    Time: 0 ms (100%), Space: 45.1 MB (71.33%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,15 +1,43 @@
+//(1)
+// class Solution {
+//     public int missingMultiple(int[] nums, int k) {
+
+//         int n = nums.length;
+//         Set<Integer> hashSet = new HashSet<>();
+
+//         for (int i : nums) {
+//             hashSet.add(i);
+//         }
+
+//         int i = 1;
+//         while (hashSet.contains(i * k)) {
+//             i++;
+
+//         }
+
+//         return (i * k);
+//     }
+// }
+
+//(2)
 class Solution {
     public int missingMultiple(int[] nums, int k) {
 
         int n = nums.length;
-        Set<Integer> hashSet = new HashSet<>();
+        int[] arr = new int[101];
 
         for (int i : nums) {
-            hashSet.add(i);
+            arr[i - 1] = 1;
+            ;
         }
 
         int i = 1;
-        while (hashSet.contains(i * k)) {
+        while (true) {
+
+            if (((k * i)) > arr.length || arr[(k * i) - 1] == 0) {
+                break;
+
+            }
             i++;
 
         }
