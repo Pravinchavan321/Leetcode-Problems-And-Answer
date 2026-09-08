2227f5569d184bace2731da6e9b4c51b8cc4f28e
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-09-09 01:34:34 +0530

    Time: 1 ms (83.58%), Space: 42.9 MB (8.36%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,17 +1,33 @@
+// (1)
+// class Solution {
+//     public int countCommas(int n) {
+
+//         if (n < 1000) {
+//             return 0;
+//         }
+//         int num = n;
+//         int countComma = 0;
+//         for (int i = 1000; i <= n; i++) {
+//             countComma++;
+
+//         }
+
+//         return countComma;
+
+//     }
+// }
+
+//(2)
 class Solution {
     public int countCommas(int n) {
 
         if (n < 1000) {
             return 0;
         }
-        int num = n;
-        int countComma = 0;
-        for (int i = 1000; i <= n; i++) {
-            countComma++;
+        
+        
 
-        }
-
-        return countComma;
+        return n - 1000 + 1;
 
     }
 }
