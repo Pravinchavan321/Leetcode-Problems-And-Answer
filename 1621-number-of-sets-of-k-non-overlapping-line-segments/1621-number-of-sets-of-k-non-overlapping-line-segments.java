f1d9da71b192e3fc23a24d0ff075a66597f4bc8d
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-09-16 23:07:14 +0530

    Time: 27 ms (72.9%), Space: 42.5 MB (79.35%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,39 +1,29 @@
 class Solution {
-    private static final long MOD = 1_000_000_007L;
+    public int numberOfSets(int n, int k) {
+        final long MOD = 1_000_000_007L;
 
-    private long modPow(long base, long exp) {
-        long result = 1;
+        long[] dp = new long[n];
+        long[] prefixSums = new long[n + 1];
 
-        while (exp > 0) {
-            if ((exp & 1L) != 0) {
-                result = result * base % MOD;
+        for (int j = 0; j < n; j++) {
+            dp[j] = 1;
+            prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+        }
+
+        for (int i = 0; i < k; i++) {
+            dp[0] = 0;
+
+            for (int j = 1; j < n; j++) {
+                dp[j] = (dp[j - 1] + prefixSums[j]) % MOD;
             }
 
-            base = base * base % MOD;
+            prefixSums[0] = 0;
 
-            exp >>= 1;
+            for (int j = 0; j < n; j++) {
+                prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+            }
         }
 
-        return result;
-    }
-
-    public int numberOfSets(int n, int k) {
-        long N = n + k - 1L;
-        long R = 2L * k;
-
-        R = Math.min(R, N - R);
-
-        long numerator = 1;
-        long denominator = 1;
-
-        for (long i = 1; i <= R; i++) {
-            numerator = numerator * (N - R + i) % MOD;
-
-            denominator = denominator * i % MOD;
-        }
-
-        long inverseDenominator = modPow(denominator, MOD - 2);
-
-        return (int) (numerator * inverseDenominator % MOD);
+        return (int) dp[n - 1];
     }
 }
