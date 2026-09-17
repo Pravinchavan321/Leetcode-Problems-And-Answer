44bb4ee767cbf14a7a75112ff516416db5219b28
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-09-17 23:27:16 +0530

    Time: 1624 ms (33.88%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,50 +1,50 @@
 -- # Write your MySQL query statement below
--- SELECT name AS results 
--- FROM (
---     SELECT mr.user_id, u.name, COUNT(*) as count
---     FROM MovieRating mr
---     LEFT JOIN Users u
---     ON mr.user_id = u.user_id
---     GROUP BY mr.user_id, name
---     ORDER BY count DESC, name ASC
---     LIMIT 1
--- ) AS highrateuser
--- UNION ALL
--- SELECT harm.title as results
--- FROM (
---     SELECT m.title, AVG(mr.rating) as avg_rating
---     FROM MovieRating mr
---     LEFT JOIN Movies m
---     ON mr.movie_id = m.movie_id
---     WHERE YEAR(mr.created_at) = YEAR('2020-02-01') AND MONTH(mr.created_at) = MONTH('2020-02-01')
---     GROUP BY m.title
---     ORDER BY avg_rating DESC,  m.title
---     LIMIT 1
--- ) AS harm
-
-
-SELECT name AS results
+SELECT name AS results 
 FROM (
-    SELECT u.name, COUNT(*) AS rating_count
+    SELECT mr.user_id, u.name, COUNT(*) as count
     FROM MovieRating mr
-    JOIN Users u
-        ON mr.user_id = u.user_id
-    GROUP BY mr.user_id, u.name
-    ORDER BY rating_count DESC, u.name ASC
+    LEFT JOIN Users u
+    ON mr.user_id = u.user_id
+    GROUP BY mr.user_id, name
+    ORDER BY count DESC, name ASC
     LIMIT 1
-) AS user_result
-
+) AS highrateuser
 UNION ALL
-
-SELECT title AS results
+SELECT harm.title as results
 FROM (
-    SELECT m.title, AVG(mr.rating) AS avg_rating
+    SELECT m.title, AVG(mr.rating) as avg_rating
     FROM MovieRating mr
-    JOIN Movies m
-        ON mr.movie_id = m.movie_id
-    WHERE mr.created_at >= '2020-02-01'
-      AND mr.created_at < '2020-03-01'
-    GROUP BY m.movie_id, m.title
-    ORDER BY avg_rating DESC, m.title ASC
+    LEFT JOIN Movies m
+    ON mr.movie_id = m.movie_id
+    WHERE YEAR(mr.created_at) = YEAR('2020-02-01') AND MONTH(mr.created_at) = MONTH('2020-02-01')
+    GROUP BY m.title
+    ORDER BY avg_rating DESC,  m.title
     LIMIT 1
-) AS movie_result;
+) AS harm
+
+
+-- SELECT name AS results
+-- FROM (
+--     SELECT u.name, COUNT(*) AS rating_count
+--     FROM MovieRating mr
+--     JOIN Users u
+--         ON mr.user_id = u.user_id
+--     GROUP BY mr.user_id, u.name
+--     ORDER BY rating_count DESC, u.name ASC
+--     LIMIT 1
+-- ) AS user_result
+
+-- UNION ALL
+
+-- SELECT title AS results
+-- FROM (
+--     SELECT m.title, AVG(mr.rating) AS avg_rating
+--     FROM MovieRating mr
+--     JOIN Movies m
+--         ON mr.movie_id = m.movie_id
+--     WHERE mr.created_at >= '2020-02-01'
+--       AND mr.created_at < '2020-03-01'
+--     GROUP BY m.movie_id, m.title
+--     ORDER BY avg_rating DESC, m.title ASC
+--     LIMIT 1
+-- ) AS movie_result;
