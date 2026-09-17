7d9602f5ff79f6a06eca1e95c87a8aed534c312b
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-09-17 19:11:57 +0530

    Time: 591 ms (65.7%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,10 +1,22 @@
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
+-- SELECT employee_id, department_id
+-- FROM Employee
+-- WHERE primary_flag = 'Y'
 
-UNION
+-- UNION
 
-SELECT employee_id, department_id
+-- SELECT employee_id, department_id
+-- FROM Employee
+-- GROUP BY employee_id
+-- HAVING COUNT(*) = 1;
+
+
+
+SELECT DISTINCT employee_id, department_id
 FROM Employee
-GROUP BY employee_id
-HAVING COUNT(*) = 1;
+WHERE employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+) 
+OR primary_flag = 'Y'
