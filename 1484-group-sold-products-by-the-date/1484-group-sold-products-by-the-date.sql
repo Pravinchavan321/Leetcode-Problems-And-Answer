8f576360331ecae929a2a3360e5622bf31b7f9f5
Chavan Pravin Pralhad <153107279+Pravinchavan321@users.noreply.github.com>
2026-09-21 13:52:32 +0530

    Time: 495 ms (57.44%), Space: 0B (100%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,12 @@
+select 
+    sell_date, 
+    COUNT( DISTINCT product ) as num_sold ,
+    
+    GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
+    
+FROM 
+    Activities 
+GROUP BY 
+    sell_date 
+order by 
+    sell_date ASC;
